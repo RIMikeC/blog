cea89f4dea4bfd089ca5f88c97248db5ba351f20
RIMikeC <mike.charles@river-island.com>
2019-01-29 14:29:18 +0000

    add-asg

--- terraform/modules/blog/main.tf
+++ terraform/modules/blog/main.tf
@@ -17,8 +17,34 @@ resource "aws_lb" "nlb" {
   enable_deletion_protection = true
 
   tags = {
-    Environment = "dev"
+    owner = "mikec"
   }
+}
+
+resource "aws_launch_template" "blog_launch_template" {
+  name                   = "blog_launch_template"
+  description            = "blog launch template"
+  iam_instance_profile   = "${aws_iam_instance_profile.ec2_instance_profile.name}"
+  image_id               = "${data.aws_ami.ami.id}"
+  instance_type          = "t2.micro"
+  key_name               = "${aws_key_pair.keys.key_name}"
+  tags                   = "${var.tags}"
+  vpc_security_group_ids = ["sg-0cb6af1b772ad904f"]
+  user_data              = "${file("${path.module}/startblog.sh")}"
+
+  monitoring {
+    enabled = true
+  }
+}
+
+resource "aws_autoscaling_group" "blog_asg" {
+  name                 = "blog asg"
+  min_size             = "1"
+  max_size             = "2"
+  launch_configuration = "${aws_launch_template.blog_launch_template.name}"
+  health_check_type    = "EC2"
+  desire_capacity      = "1"
+  load_balancers       = ["aws_lb.nlb.name"]
 }
 
 resource "aws_key_pair" "keys" {
