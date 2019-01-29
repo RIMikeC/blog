02aac746a63d7db5933c8e73f23e5c25c4d99e8c
RIMikeC <mike.charles@river-island.com>
2019-01-29 14:48:59 +0000

    typos

--- terraform/modules/blog/main.tf
+++ terraform/modules/blog/main.tf
@@ -21,29 +21,26 @@ resource "aws_lb" "nlb" {
   }
 }
 
-resource "aws_launch_template" "blog_launch_template" {
-  name                   = "blog_launch_template"
-  description            = "blog launch template"
-  iam_instance_profile   = "${aws_iam_instance_profile.ec2_instance_profile.name}"
-  image_id               = "${data.aws_ami.ami.id}"
-  instance_type          = "t2.micro"
-  key_name               = "${aws_key_pair.keys.key_name}"
-  tags                   = "${var.tags}"
-  vpc_security_group_ids = ["sg-0cb6af1b772ad904f"]
-  user_data              = "${file("${path.module}/startblog.sh")}"
+resource "aws_launch_configuration" "blog_launch_conf" {
+  name                        = "blog_launch_template"
+  iam_instance_profile        = "${aws_iam_instance_profile.ec2_instance_profile.name}"
+  image_id                    = "${data.aws_ami.ami.id}"
+  instance_type               = "t2.micro"
+  key_name                    = "${aws_key_pair.keys.key_name}"
+  associate_public_ip_address = "true"
+  security_groups             = ["sg-0cb6af1b772ad904f"]
+  user_data                   = "${file("${path.module}/startblog.sh")}"
 
-  monitoring {
-    enabled = true
-  }
+  enable_monitoring = "true"
 }
 
 resource "aws_autoscaling_group" "blog_asg" {
   name                 = "blog asg"
   min_size             = "1"
   max_size             = "2"
-  launch_configuration = "${aws_launch_template.blog_launch_template.name}"
+  launch_configuration = "${aws_launch_configuration.blog_launch_conf.name}"
   health_check_type    = "EC2"
-  desire_capacity      = "1"
+  desired_capacity     = "1"
   load_balancers       = ["aws_lb.nlb.name"]
 }
 
